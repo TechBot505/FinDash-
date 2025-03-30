6da20edfbac90cdb5d6ab0f3a1339ea879f60357
Rohit <rohitdhanotia2003@gmail.com>
2025-03-30 20:30:51 +0530

    Fixed progess component

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,15 +1,44 @@
 'use client';
 
 import * as React from 'react';
-import * as ProgressPrimitive from '@radix-ui/react-progress';
-
 import { cn } from '@/lib/utils';
 
+// Create a simple version of the Radix UI Progress component without the dependency
+const Root = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
+      className
+    )}
+    role="progressbar"
+    {...props}
+  />
+));
+Root.displayName = "ProgressRoot";
+
+const Indicator = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, style, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("h-full w-full flex-1 bg-primary transition-all", className)}
+    style={style}
+    {...props}
+  />
+));
+Indicator.displayName = "ProgressIndicator";
+
+// Recreate the same API as the original Progress component
 const Progress = React.forwardRef<
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root> & { value?: number }
 >(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
+  <Root
     ref={ref}
     className={cn(
       'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
@@ -17,12 +46,12 @@ const Progress = React.forwardRef<
     )}
     {...props}
   >
-    <ProgressPrimitive.Indicator
+    <Indicator
       className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
-  </ProgressPrimitive.Root>
+  </Root>
 ));
-Progress.displayName = ProgressPrimitive.Root.displayName;
+Progress.displayName = "Progress";
 
 export { Progress };
